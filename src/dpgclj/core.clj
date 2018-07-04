8529968f990e53470c011d8a691d053f6a7152a9
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-04 22:34:01 +0200

    reading diceware lists

--- src/dpgclj/core.clj
+++ src/dpgclj/core.clj
@@ -1,19 +1,61 @@
 (ns dpgclj.core
   (:gen-class))
 
-(def polish_list (slurp "diceware-pl.txt"))
-(def english_list (slurp "diceware-en.txt"))
+(def diceware_file_polish (slurp "diceware-pl.txt"))
+(def diceware_file_english (slurp "diceware-en.txt"))
 
 (defn get-all-lines [source]
   (clojure.string/split-lines source))
 
+(defstruct diceware-repository :words :length :dices :language)
+(defstruct repository :en :pl)
+
+(defn extract-words [words-list]
+  (def lines (clojure.string/split-lines words-list))
+  (for [w lines :let [words (last (clojure.string/split w #"\t"))]] words)
+  ;; (for [w (extract-words english_list) :let [words (last (clojure.string/split w #"\t"))]] words)
+  ;; (for [w (extract-words english_list) :let [words (last (clojure.string/split w #"\t"))]] words)
+  )
+
+(defn create-repository []
+  (def lines_pl (get-all-lines diceware_file_polish))
+  (def lines_en (get-all-lines diceware_file_english))
+  ;;(def first_english (first lines_en))
+  ;;(def first_polish (first lines_pl))
+  ;;(print "first polish word: ")
+  ;;(println first_polish)
+  ;;(print "first english word: ")
+  ;;(println first_english)
+  (def polish-repository (struct-map diceware-repository
+                                     :words lines_pl
+                                     :length (count lines_pl)
+                                     :dices 0
+                                     :language "pl"))
+  ;;(def english-repository (struct-map diceware-repository
+  ;;                                    :words (extract-words lines_en)
+  ;;                                    :length (count lines_en)
+  ;;                                    :dices 0
+  ;;                                    :language "en"))
+  ;;(vector polish-repository english-repository)
+  (def repository (vector polish-repository))
+  repository
+  )
+
+(defn get-repo-by-language [language]
+                      (println "this is not implemented yet!"))
+
+(defn diceware-info [diceware]
+  (println "language: " (str (get diceware :language)))
+  (println "first word: " (
+                           str(first(get diceware :words))))
+  (println "dices: " (str (get diceware :dices)))
+  (println "lenght: " (str (get diceware :length)))
+  )
+
+
 (defn -main
   [& args]
-  (def all_lines_polish (get-all-lines polish_list))
-  (def all_lines_english (get-all-lines english_list))
-  (def first_english (first all_lines_english))
-  (def first_polish (first all_lines_polish))
-  (println "first polish word:")
-  (println first_polish)
-  (println "first english word:")
-  (println first_english))
+  (def repository (create-repository))
+  (diceware-info (first repository))
+  (diceware-info (last repository))
+  )
