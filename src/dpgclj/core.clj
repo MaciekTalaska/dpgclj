9678a78543814125b66d00c77f4d23e3e075b18d
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-09 00:34:25 +0200

    naive way of parsing/reading command line arguments

--- src/dpgclj/core.clj
+++ src/dpgclj/core.clj
@@ -2,20 +2,32 @@
   (:gen-class)
   (:require [dpgclj.diceware :as dw])
   (:require [dpgclj.password :as pass])
+  (:require [dpgclj.options :as opt])
   )
 
 
 (defn -main
   [& args]
+
+  (def options (opt/get-options args))
   (def repository (dw/create-repositories))
 
-  ;; according to https://stackoverflow.com/a/8536695
-  ;; doseq should be used instead of map/for when dealing with side-effects
-  ;; (doseq [r repository] (dw/diceware-info r))
-  (println "single random word:\n"
-           (pass/get-random-word "pl" repository))
-  (println "single 5-word password:\n"
-           (pass/get-random-words "pl" repository 5))
-  (println "5 word password with . as separator:\n" (pass/create-password "pl" repository 5 "."))
-  (println "3 5-words passwords using . as separator:\n" (pass/create-all-passwords "pl" repository 5 "." 3))
+  (println (pass/create-all-passwords
+            (options :language)
+            repository
+            (options :words)
+            (options :separator)
+            (options :passwords)))
+  ; (def options (opt/get-options args))
+;;   (def repository (dw/create-repositories))
+;; 
+;;   ;; according to https://stackoverflow.com/a/8536695
+;;   ;; doseq should be used instead of map/for when dealing with side-effects
+;;   ;; (doseq [r repository] (dw/diceware-info r))
+;;   (println "single random word:\n"
+;;            (pass/get-random-word "pl" repository))
+;;   (println "single 5-word password:\n"
+;;            (pass/get-random-words "pl" repository 5))
+;;   (println "5 word password with . as separator:\n" (pass/create-password "pl" repository 5 "."))
+;;   (println "3 5-words passwords using . as separator:\n" (pass/create-all-passwords "pl" repository 5 "." 3))
   )
---
+++ src/dpgclj/options.clj
@@ -0,0 +1,55 @@
+(ns dpgclj.options)
+
+(defstruct dpg-options
+  :language
+  :words-count
+  :separator
+  :passwords-count)
+
+(defn print-help []
+  (println "Diceware Password Generator (Clojure version)")
+  (println "2018 by Maciek Talaska")
+  (println "sources: https://github.com/MaciekTalaska/dpgclj")
+  (println "")
+  (println "options:")
+  (println "-l:language - specifiy language list (en/pl)")
+  (println "-w:words - number of words password should be build of")
+  (println "-s:separator - char to be used as words separator")
+  (println "-p:passwords - number of passwords to generate")
+  )
+
+(defn get-language [input]
+  (def language-option(re-matches #"(.*)(-l:)([a-z]{2})(.*)" input))
+  {:language (language-option 3)}
+  )
+
+(defn get-words-count [input]
+  (def words-option (re-matches #"(.*)(-w:)([0-9]*)(.*)" input))
+  {:words (read-string (words-option 3))}
+  )
+
+(defn get-passwords-count [input]
+  (def passwords-option (re-matches #"(.*)(-p:)([0-9*])(.*)" input))
+  {:passwords (read-string(passwords-option 3))}
+  )
+
+(defn get-separator [input]
+  (def separator-option (re-matches #"(.*)(-s:)(.)(.*)" input))
+  {:separator (separator-option 3)}
+  )
+
+(defn get-options [& args]
+  (def args-string (clojure.string/join " " args))
+  (merge
+   (get-language args-string)
+   (get-words-count args-string)
+   (get-separator args-string)
+   (get-passwords-count args-string)
+   ()
+   )
+ )
+
+(defn test-options []
+  (def all-options (get-options "-l:pl" "-w:5" "-s:*" "-p:3"))
+  all-options
+  )
