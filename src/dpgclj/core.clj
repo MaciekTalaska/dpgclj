4e6e0953888b689af9030a509f894c6fccdfa3be
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-15 19:00:50 +0200

    core: refactor (eastwood)

--- src/dpgclj/core.clj
+++ src/dpgclj/core.clj
@@ -10,18 +10,15 @@
   [& args]
 
   (if-not (empty? args)
-    (
-     (def options (opt/get-options args))
-     (opt/exit-if-invalid-options options)
-
-     (def repository (dw/create-repositories))
-
-     (println (pass/create-all-passwords
-              (options :language)
-              repository
-              (options :words)
-              (options :separator)
-              (options :passwords)))
+    ((let [options (opt/get-options args)]
+       (opt/exit-if-invalid-options options)
+       (let [repository (dw/create-repositories)]
+         (println (pass/create-all-passwords
+                   (options :language)
+                   repository
+                   (options :words)
+                   (options :separator)
+                   (options :passwords)))))
      ; TODO: investigate why uncommenting the line below
      ; results in run-time error when proper parameters are passed
      ; The error occrus at the very end, when all processing is done,
@@ -29,10 +26,6 @@
      ; Note: this is linked to calling opt/exit-if-invalid-options -
      ; if the mentioned function is not called, line below could
      ; be omitted
-     (System/exit 0)
-     )
-    (
-     (opt/print-help)
-     (System/exit 1))
-    )
-  )
+     (System/exit 0))
+    ((opt/print-help)
+     (System/exit 1))))
