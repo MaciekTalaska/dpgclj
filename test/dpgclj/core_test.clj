a1b47a277dcee2438bfc05f0201afdcbcee89ea4
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-24 23:03:46 +0200

    disabling test for core

--- test/dpgclj/core_test.clj
+++ test/dpgclj/core_test.clj
@@ -1,7 +1,7 @@
-(ns dpgclj.core-test
-  (:require [clojure.test :refer :all]
-            [dpgclj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;(ns dpgclj.core-test
+;  (:require [clojure.test :refer :all]
+;            [dpgclj.core :refer :all]))
+;
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
