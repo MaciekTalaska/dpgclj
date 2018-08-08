5c52009c3a5ad91ec57cdec30aeb641b93a17603
MaciekTalaska <maciek.talaska@gmail.com>
2018-08-08 16:51:40 +0200

    unused code removed

--- test/dpgclj/options_test.clj
+++ test/dpgclj/options_test.clj
@@ -6,21 +6,6 @@
   (testing "simple-test-options"
     (is (= 10 10))))
 
-;(deftest simple-test
-;  (testing "something"
-;    (with-redefs-fn [exit-with-message (fn [code message] constantly "weh")]
-;      (is (= "weh" (check-required-options ["-x"]))))))
-
-;(deftest simple-test-2
-;  (testing "something different"
-;    (with-redefs [exit-now1! (constantly "weh")]
-;      (is (= "weh" (check-required-options ["-x"]))))))
-
-;(deftest simple-test-2
-;  (testing "something different"
-;    (with-redefs [exit-now! (fn [code] (str "weh"))]
-;      (is (= "weh" (check-required-options ["-x"]))))))
-
 ;(deftest l1ack-of-required-option-results-in-error-message
 (deftest first-test
   (testing "first"
@@ -32,14 +17,5 @@
     (with-redefs [exit-with-message! (fn [code message] {:code code :message message})]
       (let [expected-code 1
             expected-message "both '-l' and '-w' have to be provided!"
-            ;returned (check-required-options ["-x"])
-            ;returned-code (:code returned)
-            ;returned-message (:message returned)
-            expected {:code expected-code :message expected-message}
-            ]
+            expected {:code expected-code :message expected-message}]
         (is (= expected (check-required-options ["-x"])))))))
-
-;        (is (= {:code expected-code :message expected-message} {:code returned-code :message returned-message})))))
-;        )
-;      (is (= {:code 1 :message "both '-l' and '-w' have to be provided!"} {:code 1 :message "both '-l' and '-w' have to be provided!"})))))
-;      (is (= {:code 1 :message "both '-l' and '-w' have to be provided!"} (check-required-options ["-x"]))))))
