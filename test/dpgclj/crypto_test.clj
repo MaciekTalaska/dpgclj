5ae5da3acb4e00d3706174c7ca7a690e86412500
MaciekTalaska <maciek.talaska@gmail.com>
2018-08-10 17:17:17 +0200

    crypto tests improved

--- test/dpgclj/crypto_test.clj
+++ test/dpgclj/crypto_test.clj
@@ -3,5 +3,11 @@
             [dpgclj.crypto :refer :all]))
 
 (deftest random-value-should-be-smaller-than-max
-  (testing "returned value is not smaller than max!"
-    (is (< (secure-rand-int 17) 17))))
+  (testing "(secure-rand-int 1) must return 0"
+    (is (= (secure-rand-int 1) 0)))
+  (testing "(secure-rand-int 2) must return 0 or 1"
+    (is (< (secure-rand-int 2) 2)))
+  (testing "(secure-rand-int X) must return at most X-1"
+    (let [max (secure-rand-int 255)]
+      (is (< (secure-rand-int max) max)))))
+
