3a582259b8cb9180dc5caf6022ff02151f9e0d91
MaciekTalaska <maciek.talaska@gmail.com>
2018-08-12 17:54:38 +0200

    fix: no exception when not using System/exit is being thrown

--- src/dpgclj/core.clj
+++ src/dpgclj/core.clj
@@ -9,7 +9,7 @@
   [& args]
 
   (if-not (empty? args)
-    ((let [options (opt/get-options args)]
+    (let [options (opt/get-options args)]
        (opt/exit-if-invalid-options options)
        (let [repository (dw/create-repositories)]
          (println (pass/create-all-passwords
@@ -18,13 +18,4 @@
                    (options :words)
                    (options :separator)
                    (options :passwords)))))
-     ; TODO: investigate why uncommenting the line below
-     ; results in run-time error when proper parameters are passed
-     ; The error occrus at the very end, when all processing is done,
-     ; and the generated password is already printed to the console.
-     ; Note: this is linked to calling opt/exit-if-invalid-options -
-     ; if the mentioned function is not called, line below could
-     ; be omitted
-     (System/exit 0))
-    ((opt/print-help)
-     (System/exit 1))))
+    (opt/print-help)))
