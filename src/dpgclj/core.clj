e5b00741a4309be8931354a1f12a0b62992ba234
MaciekTalaska <maciek.talaska@gmail.com>
2018-08-20 12:54:21 +0200

    core.clj: refactor

--- src/dpgclj/core.clj
+++ src/dpgclj/core.clj
@@ -4,18 +4,16 @@
   (:require [dpgclj.password :as pass])
   (:require [dpgclj.options :as opt]))
 
-
 (defn -main
   [& args]
-
   (if-not (empty? args)
-    (let [options (opt/get-options args)]
-       (opt/exit-if-invalid-options options)
-       (let [repository (dw/create-repositories)]
-         (println (pass/create-all-passwords
-                   (options :language)
-                   repository
-                   (options :words)
-                   (options :separator)
-                   (options :passwords)))))
+    (let [options (opt/get-options args)
+          _ (opt/exit-if-invalid-options options)
+          repository (dw/create-repositories)]
+      (println (pass/create-all-passwords
+                (options :language)
+                repository
+                (options :words)
+                (options :separator)
+                (options :passwords))))
     (opt/print-help)))
